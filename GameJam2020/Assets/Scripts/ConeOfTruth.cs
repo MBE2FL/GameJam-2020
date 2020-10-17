37beefdb523fd8cc790d338b6def3565b900a95f
Max Kemp <max.kemp@ontariotechu.net>
2020-10-17 17:45:58 -0400

    Added in fading of objects for Cone of Truth.

--- GameJam2020/Assets/Scripts/ConeOfTruth.cs
+++ GameJam2020/Assets/Scripts/ConeOfTruth.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,6 +8,11 @@ public class ConeOfTruth : MonoBehaviour
 {
     [SerializeField]
     float _radius = 10.0f;
+    [SerializeField, Range(0.0f, 5.0f)]
+    float _fadeRadiusMultiplier = 1.5f;
+    [SerializeField, Range(0.0f, 0.25f)]
+    float _fadeTheshold = 0.05f;
+    [SerializeField]
     List<Collider> _collidersInRange = new List<Collider>();
 
     // Start is called before the first frame update
@@ -18,53 +24,66 @@ public class ConeOfTruth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Disable visibility of all previous frame's objects.
-        foreach (Collider collider in _collidersInRange)
+        // Adjust the visibility of all the previous frame's objects.
+        for (int i = 0; i < _collidersInRange.Count; ++i)
         {
+            Collider collider = _collidersInRange[i];
+
             MeshRenderer meshRenderer = collider.gameObject.GetComponent<MeshRenderer>();
 
             if (meshRenderer)
             {
-                meshRenderer.enabled = false;
+                Vector3 playerToObj = collider.transform.position - transform.position;
+
+                float fade = ((_radius * _fadeRadiusMultiplier) - playerToObj.magnitude) / (_radius);
+                fade = Mathf.Clamp01(fade);
+
+                meshRenderer.material.SetFloat("Fade", fade);
+
+                // Remove collider and set visibility to zero, when below fade theshold.
+                if (fade < _fadeTheshold)
+                {
+                    meshRenderer.material.SetFloat("Fade", 0.0f);
+
+                    //_collidersInRange.RemoveAt(i);
+
+                    Collider temp = _collidersInRange[_collidersInRange.Count - 1];
+                    _collidersInRange[i] = temp;
+                    _collidersInRange.RemoveAt(_collidersInRange.Count - 1);
+
+                    --i;
+                }
+
+                Debug.DrawRay(transform.position, playerToObj.normalized * _radius, Color.green);
             }
         }
 
-        _collidersInRange.Clear();
 
-
-        // Find all the objects in range for this frame, and enable their visibility.
+        // Find all the objects in range for this frame.
         int layerMask = 1 << 9;
-        Collider[] colliders = Physics.OverlapSphere(transform.position, _radius, layerMask);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _radius * _fadeRadiusMultiplier, layerMask);
+
+        //colliders = _collidersInRange.
 
         foreach (Collider collider in colliders)
         {
             // Check if the object is unobstructed from the player's view.
-            Vector3 playerToObj = collider.transform.position - transform.position;
-            Vector3 playerToObjDir = playerToObj.normalized;
+            Vector3 playerToObjDir = (collider.transform.position - transform.position).normalized;
             RaycastHit hit;
 
-            if (Physics.Raycast(transform.position, playerToObjDir, out hit, _radius))
+            if (Physics.Raycast(transform.position, playerToObjDir, out hit, _radius * _fadeRadiusMultiplier))
             {
                 if (collider != hit.collider)
                 {
                     continue;
                 }
 
-                MeshRenderer meshRenderer = collider.gameObject.GetComponent<MeshRenderer>();
-
-                if (meshRenderer)
+                if (!_collidersInRange.Contains(collider))
                 {
-                    meshRenderer.enabled = true;
+                    _collidersInRange.Add(collider);
                 }
-
-                _collidersInRange.Add(collider);
-
-                Debug.DrawRay(transform.position, playerToObjDir * _radius, Color.green);
             }
         }
-
-        //_collidersInRange = new List<Collider>(colliders);
-
 
 
         Debug.DrawRay(transform.position, transform.forward * _radius, Color.red);
