4bcbbf3bb5b05b31952899ceeb0abe2d7025c84b
Emory <emory.wynn@ontariotechu.net>
2020-10-17 16:04:26 -0400

    Photon Testing

---
+++ GameJam2020/Assets/Scripts/Player Lobby.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLobby : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+
+    }
+
+    void connection ()
+    {
+
+    }
+}
