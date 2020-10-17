e8d59194cc4d543a277e2e2e505874b7c147e55a
NoahColangelo <noah.colangelo@uoit.net>
2020-10-17 11:37:03 -0400

    added mach player model and basic movement

---
+++ GameJam2020/Assets/Scripts/movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody _rb;
+
+    public float speedMultiplier = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _rb.AddForce(Vector3.forward * speedMultiplier);
+
+        if (Input.GetKey(KeyCode.A))
+            _rb.AddForce(Vector3.left * speedMultiplier);
+
+        if (Input.GetKey(KeyCode.S))
+            _rb.AddForce(Vector3.back * speedMultiplier);
+
+        if (Input.GetKey(KeyCode.D))
+            _rb.AddForce(Vector3.right * speedMultiplier);
+    }
+}
