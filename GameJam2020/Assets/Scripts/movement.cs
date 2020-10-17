49364de92cf79e65138533677338c60266337486
NoahColangelo <noah.colangelo@uoit.net>
2020-10-17 14:09:42 -0400

    adds in the rotation for the player based off the mouse position

--- GameJam2020/Assets/Scripts/movement.cs
+++ GameJam2020/Assets/Scripts/movement.cs
@@ -7,9 +7,9 @@ public class movement : MonoBehaviour
     [SerializeField]
     Rigidbody _rb;
 
-    public float speedMultiplier = 2.0f;
+    public float speedMultiplier = 10.0f;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +20,14 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        LookAtMouse();
 
     }
 
     private void FixedUpdate()
     {
+        //basic movement system for the player using the rigidbody
+
         Vector3 force = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W))
@@ -40,5 +43,19 @@ public class movement : MonoBehaviour
             force += Vector3.right;
 
         _rb.AddForce(force.normalized * speedMultiplier);
+    }
+
+    private void LookAtMouse()
+    {
+        //this function shoots a ray from where the mouse is in pixel space down to 3d space
+        //if it detects a hit on the ground the plyaers y rotation will face toward the position of the hit
+
+        RaycastHit hit;
+        Ray mouseCoord = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(mouseCoord, out hit, 100.0f, ~10))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
     }
 }
