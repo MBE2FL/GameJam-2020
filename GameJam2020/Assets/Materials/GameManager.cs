02e6b0e01ddb08828059e093f4d30026a1e0889d
Max Kemp <max.kemp@ontariotechu.net>
2020-10-17 22:10:44 -0400

    Game manager will make all fade materials non-visible upon game start.

---
+++ GameJam2020/Assets/Materials/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    List<Material> _fadeMaterials;
+
+
+    private void Awake()
+    {
+        // Set all fade materials to not be visible on game start.
+        foreach (Material material in _fadeMaterials)
+        {
+            material.SetFloat("Fade", 0.0f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
