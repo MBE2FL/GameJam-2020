28af7dafec3dcababc0023f578a8edb2664010b7
NoahColangelo <noah.colangelo@uoit.net>
2020-10-17 20:00:12 -0400

    Added in background sound and player footsteps

---
+++ GameJam2020/Assets/Scripts/PlayerFootsteps.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    List<AudioClip> footsteps;
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.GetComponent<Rigidbody>().velocity.x > 0.5f || player.GetComponent<Rigidbody>().velocity.x < -0.5f ||
+           player.GetComponent<Rigidbody>().velocity.z > 0.5f || player.GetComponent<Rigidbody>().velocity.z < -0.5f)
+        {
+            //play footsteps
+            if(!audioSource.isPlaying)
+            {
+                int rand = Random.Range(0, 3);
+
+                audioSource.clip = footsteps[rand];
+                audioSource.PlayDelayed(0.2f);
+                //Debug.Log("it be playing");
+                //choose next sound and play. maybe add timer for cooldown
+            }
+        }
+    }
+}
